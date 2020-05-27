fd6a51df26466d8c9d0ca2f1c8805e68fe53d8a6
Tatiane Gabrielle <shinoda_@hotmail.co.uk>
2020-05-26 23:10:36 -0300

    Menu fixo ao rolar

--- js/script.js
+++ js/script.js
@@ -5,3 +5,16 @@ function openNav() {
   function closeNav() {
     document.getElementById("menu-celular").style.width = "0";
   }
+
+//Sticky on Scroll 
+window.onscroll = function() {myFunction()};
+  var navbar = document.getElementById("navbar");
+  var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
